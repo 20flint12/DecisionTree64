39d6121179f53633ae15808640c66d715d462c76
Sergey <20flint12@gmail.com>
2023-05-05 18:18:30 +0200

    tmix script++++++

--- ptb_main_astro.sh
+++ ptb_main_astro.sh
@@ -6,10 +6,10 @@ tmux attach-session -t ptb_main_astro
 # You are in the root ...
 cd /home/ubuntu/DecisionTree/
 
-echo "--- update repo ---"
-git fetch
-git switch develop
-git pull origin develop
+#echo "--- update repo ---"
+#git fetch
+#git switch develop
+#git pull origin develop
 
 echo "--- run env ---"
 source venv310/bin/activate
