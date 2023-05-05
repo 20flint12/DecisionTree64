40cf705405fd9ff74712353e28999efd17f4942f
Sergey <20flint12@gmail.com>
2023-05-05 19:40:20 +0200

    tmix script+++++++++++

--- ptb_main_astro.sh
+++ ptb_main_astro.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-#cd /home/ubuntu/DecisionTree64
+cd /home/ubuntu/DecisionTree64
+read
 
 echo "--- attach session ---"
 tmux attach-session -t ptb_main_astro
+read
 
 # You are in the root ...
 cd /home/ubuntu/DecisionTree64
+read
 
 echo "--- run env ---"
 source venv310/bin/activate
+read
 
 echo "--- run ptb_main_astro ---"
 python -m src.PTB_bot.ptb_main_astro
--- ptb_main_recorder.sh
+++ ptb_main_recorder.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+cd /home/ubuntu/DecisionTree64
+read
+
 echo "--- attach session ---"
 tmux attach-session -t ptb_main_recorder
+read
 
 # You are in the root ...
 cd /home/ubuntu/DecisionTree64
+read
 
 echo "--- run env ---"
 source venv310/bin/activate
+read
 
 echo "--- run ptb_main_recorder ---"
 python -m src.PTB_bot.ptb_main_recorder
