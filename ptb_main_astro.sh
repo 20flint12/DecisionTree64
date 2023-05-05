8c16fc80e02b1b01619f362aeaead089478402cf
Sergey <20flint12@gmail.com>
2023-05-05 17:40:53 +0200

    tmix script+

---
+++ ptb_main_astro.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "--- attach session ---"
+tmux attach-session -t ptb_main_astro
+
+# You are in the root ...
+cd /home/ubuntu/DecisionTree/
+
+echo "--- update repo ---"
+git fetch
+git switch develop
+git pull origin develop
+
+echo "--- run env ---"
+source venv310/bin/activate
+
+echo "--- run python script ---"
+python -m src.PTB_bot.ptb_main_astro
+
--- tmix_sessions.sh
+++ tmix_sessions.sh
@@ -7,8 +7,11 @@ chmod +x ./BinanceTrader/ptb_binance_monitor.sh
 
 echo "--- open session ---"
 tmux new -s ptb_main_astro
+tmux detach
 tmux new -s ptb_main_recorder
+tmux detach
 tmux new -s ptb_binance_monitor
+tmux detach
 #tmux a -t ptb_binance_monitor
 
 echo "--- check sessions ---"
