cb205e4679398eb6c5c72977a886bc786cccf6ab
Sergey <20flint12@gmail.com>
2023-05-05 17:47:13 +0200

    tmix script+++

--- tmux_sessions.sh
+++ tmux_sessions.sh
@@ -6,12 +6,12 @@ chmod +x ./DecisionTree64/ptb_main_recorder.sh
 chmod +x ./BinanceTrader/ptb_binance_monitor.sh
 
 echo "--- open session ---"
-tmux new -s ptb_main_astro
-tmux detach
-tmux new -s ptb_main_recorder
-tmux detach
-tmux new -s ptb_binance_monitor
-tmux detach
+tmux new -d -s ptb_main_astro
+#tmux detach
+tmux new -d -s ptb_main_recorder
+#tmux detach
+tmux new -d -s ptb_binance_monitor
+#tmux detach
 #tmux a -t ptb_binance_monitor
 
 echo "--- check sessions ---"
